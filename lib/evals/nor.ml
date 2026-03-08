c70058e293aeb936b690de06a1c9286ed1e7ec51
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2026-03-09 00:31:13 +0100

    fix: nor well implemented, fac and sum run well

--- lib/evals/nor.ml
+++ lib/evals/nor.ml
@@ -1,14 +1,14 @@
 open Core.Syntax
 open Core.Subst
+open Bn
 
 let rec eval_nor = function
-  | App (Abs (x, body), arg) ->
-      eval_nor (subst body x arg)
-
-  | App (t1, t2) ->
-      let t1' = eval_nor t1 in
-      if t1' <> t1 then eval_nor (App (t1', t2))
-      else App (t1, eval_nor t2)
+    | App (t1, t2 ) ->
+      let t1' = eval_bn t1 in (
+        match t1' with
+        | Abs(x, body) -> eval_nor @@ subst body x t2
+        | _ -> App (eval_nor t1', eval_nor t2)
+    )
 
   | Abs (x, t) ->
       Abs (x, eval_nor t)
