036e6713cb0a8c172f8bbaf997e5f257afd488fe
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2026-03-13 01:16:57 +0100

    feat: update new version

--- bin/lambda_repl.ml
+++ bin/lambda_repl.ml
@@ -7,6 +7,6 @@ let initial_config = {
 }
 
 let () =
-  print_endline @@ "λ-REPL evaluators-exploring, version 0.1.0:  " ^
+  print_endline @@ "λ-REPL evaluators-exploring, version 0.1.1:  " ^
     ":h for help,  :q for exit";
   loop initial_config
