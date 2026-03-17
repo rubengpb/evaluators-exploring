9f1394b472f3a79ae0da28d2e0800467920f8dff
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2026-03-17 01:15:22 +0100

    feat: second idea wiht readback function apply

--- lib/evals/clousure/eval_cl.ml
+++ lib/evals/clousure/eval_cl.ml
@@ -1,31 +1,40 @@
 open Core.Syntax
+open Core.Utils
 
-let rec eval_cl = function
-  | Clou (CVar x as v, Nihil) -> v
-  | Clou (CVar x as v, Bind (y, v', cont)) ->
+let rec clean_cntx x cntx =
+  match cntx with
+    | Nihil -> Nihil
+    | Bind (y, t, c) ->
+      if y = x then clean_cntx x c
+      else Bind (y, t, clean_cntx x c)
+
+let rec eval_cl cntx t =
+  match t, cntx with
+  | CVar x, Nihil -> CVar x
+  | CVar x, Bind (y, v', c) ->
     if x = y then v'
-    else eval_cl @@ Clou (v, cont)
-  | Clou (CAbs (x,b), cont) -> Clou (CAbs(x, b), cont)
-  | Clou (CApp (t1, t2), cont) ->
-    eval_cl @@ CApp( eval_cl @@ Clou (t1, cont), eval_cl @@ Clou(t2, cont))
-  | CApp (Clou(CAbs(x, b),cont), v) -> eval_cl @@ Clou(b, Bind(x, v, cont))
-  | CApp (_, _) as t -> t
+    else eval_cl c (CVar x)
+  | CAbs (x, b), c ->
+    Clou (CAbs(x, eval_cl (clean_cntx x c) b), c)
+  | CApp (t1, t2), cont ->
+    apply (eval_cl cont t1) (eval_cl cont t2)
   | _ -> failwith "Error: the cterm cannot be reduced"
+  and apply f v =
+    match f with
+    | Clou (CAbs (x, b), c) -> eval_cl (Bind (x, v, c)) b
+    | _ -> CApp (f, v)
 
-  let rec pure_of_clousure = function
+let rec pure_of_clousure = function
   | CVar x -> Var x
   | CAbs (x, b) -> Abs (x, pure_of_clousure b)
   | CApp (t1, t2) -> App (pure_of_clousure t1, pure_of_clousure t2)
   | Clou (t, Nihil) -> pure_of_clousure t
   | Clou (t, Bind(_, _, _)) -> failwith "Error: context non empthy"
 
-let rec clousure_of_pure_aux = function
+let rec clousure_of_pure = function
     | Var x -> CVar x
-    | Abs (x, b) -> CAbs (x, clousure_of_pure_aux b)
-    | App (t1, t2) -> CApp (clousure_of_pure_aux t1, clousure_of_pure_aux t2)
-
-let clousure_of_pure t =
-  Clou (clousure_of_pure_aux t, Nihil)
+    | Abs (x, b) -> CAbs (x, clousure_of_pure b)
+    | App (t1, t2) -> CApp (clousure_of_pure t1, clousure_of_pure t2)
 
 let eval_cl_pure t =
-  t |> clousure_of_pure |> eval_cl |> pure_of_clousure
+  t |> clousure_of_pure |> (eval_cl Nihil) |> pure_of_clousure
