4fb457e09c8ab960c4458319103085a8879585e5
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2026-03-19 14:08:44 +0100

    fix: print parenthesis when we print clousures

--- lib/core/utils.ml
+++ lib/core/utils.ml
@@ -12,6 +12,7 @@ let rec string_of_cterm = function
   | CApp (CApp(t1, t2), t) -> string_of_cterm (CApp (t1, t2)) ^ " (" ^ string_of_cterm t ^ ")"
   | CApp (t, CVar x) -> "(" ^ string_of_cterm t ^ ") " ^ x
   | CApp (t1, t2) -> "(" ^ string_of_cterm t1 ^ ") (" ^ string_of_cterm t2 ^ ")"
+  | Clou (CAbs(x, t), ctxt) -> "(\\" ^ x ^ "." ^ string_of_cterm t ^ " [" ^ string_of_ctxt ctxt ^ "])"
   | Clou (t, ctxt) -> string_of_cterm t ^ " [" ^ string_of_ctxt ctxt ^ "]"
 and string_of_ctxt ctxt =
   let subs = List.map (fun (v, ct) -> v ^ " -> " ^ string_of_cterm ct) ctxt in
