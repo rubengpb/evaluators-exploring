3206a23c5242fe14579a3a7dfb70e755c8c2f5ca
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2026-04-16 13:55:21 +0200

    feat: general function for string_of_term_with_options

--- lib/repl/handlers/term.ml
+++ lib/repl/handlers/term.ml
@@ -9,47 +9,28 @@ open Core.Church_numerals
 open Core.Church_list
 open Core.Syntax
 
-let rec string_of_pterm_with_options num list t =
+let rec string_of_term_with_options num list t =
   match num, list with
     | true, true -> (
-      match int_of_pterm t with
+      match int_of_term t with
         | Some n -> string_of_int n
-        | _ -> (match list_of_pterm t with
-                | Some xs -> string_of_pterm_list true xs
-                | _ -> string_of_pterm t)
+        | _ -> (match list_of_term t with
+                | Some xs -> string_of_term_list true xs
+                | _ -> string_of_term t)
       )
     | true, false -> (
-      match int_of_pterm t with
+      match int_of_term t with
         | Some n -> string_of_int n
-        | _ -> string_of_pterm t
+        | _ -> string_of_term t
     )
     | false, true -> (
-      match list_of_pterm t with
-        | Some xs -> string_of_pterm_list false xs
-        | _ -> string_of_pterm t
+      match list_of_term t with
+        | Some xs -> string_of_term_list false xs
+        | _ -> string_of_term t
     )
-    | _ -> string_of_pterm t
-and string_of_pterm_list num xs =
-  "[" ^ List.fold_left (fun acc s -> acc ^ string_of_pterm_with_options num true s ^ "; ") "" xs ^ "]"
-
-let string_of_dbterm_with_options num t =
-  if num then
-    match int_of_dbterm t with
-      | Some n -> string_of_int n
-      | None -> string_of_dbterm_pure t
-  else string_of_dbterm_pure t
-
-let string_of_cterm_with_options num t =
-  if num then
-    match int_of_cterm t with
-      | Some n -> string_of_int n
-      | None -> string_of_cterm t
-  else string_of_cterm t
-
-let string_of_term_with_options num list = function
-  | TPure tt -> string_of_pterm_with_options num list tt
-  | TDeBruijn dbt -> string_of_dbterm_with_options num dbt
-  | TClousure clt -> string_of_cterm_with_options num clt
+    | _ -> string_of_term t
+and string_of_term_list num xs =
+  "[" ^ List.fold_left (fun acc s -> acc ^ string_of_term_with_options num true s ^ "; ") "" xs ^ "]"
 
 let handle_term st t =
   let t = expand st.env t in
