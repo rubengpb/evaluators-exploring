2696305b08f789a8ac887449a9b328beb541772a
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2026-04-22 14:25:50 +0200

    feat: ao bn and no in smallstep with clousures

--- lib/evals/pure/clousure/smallstep/bn.ml
+++ lib/evals/pure/clousure/smallstep/bn.ml
@@ -1,1 +1,29 @@
-let bn t = failwith "TODO"
+open Core.Syntax
+open Core.Utils
+open Core.Forms
+open Printer
+
+let rec step_bn = function
+  | Clou(CVar x, []) -> CVar x
+  | Clou(CVar x, (y, n)::env) ->
+    if x = y then n
+    else step_bn (Clou(CVar x, env))
+  | CApp(m, n) when not (is_cwhnf m) ->
+    let m' = step_bn m in
+    CApp(m', n)
+  | CApp (CAbs(x, b), n) ->
+    Clou(b, [(x, n)])
+  | CApp (Clou(CAbs(x, b), env), n) ->
+    Clou(b, (x, n)::env)
+  | Clou(CApp(m, n), env) ->
+    step_bn (CApp(Clou(m, env), Clou(n, env)))
+  | _ -> failwith "Not redex!"
+
+let rec string_of_term_ss_bn = string_of_cterm
+
+let rec bn t =
+  if is_cwhnf t then t
+  else (
+    print_endline @@ string_of_term_ss_bn t;
+    t |> step_bn |> bn
+  )
