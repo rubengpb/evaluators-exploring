726b013675429557cf764515d33251fde01ea569
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2026-04-23 12:43:19 +0200

    feat: readback evaluators for lambda-value

--- lib/evals/value/subst/readback/gen.ml
+++ lib/evals/value/subst/readback/gen.ml
@@ -1,1 +1,8 @@
-let gen t = failwith "TODO"
+open Core.Syntax
+
+let rec gen la ar2 = function
+  | Var x -> Var x
+  | Abs (x, b) ->
+    let b' = la b in
+    Abs(x, b')
+  | App (m, n) -> App (gen la ar2 m, ar2 n)
--- lib/evals/value/subst/readback/rbun.ml
+++ lib/evals/value/subst/readback/rbun.ml
@@ -1,1 +1,11 @@
-let rbun t = failwith "TODO"
+open Core.Syntax
+open Evalapply.Bv
+
+let rec bodies3 = function
+  | Var x -> Var x
+  | Abs (x, b) ->
+    let b' = bv b in
+    Abs(x, b')
+  | App (m, n) -> App (bodies3 m, bodies3 n)
+
+let rbun t = bodies3 @@ bv t
