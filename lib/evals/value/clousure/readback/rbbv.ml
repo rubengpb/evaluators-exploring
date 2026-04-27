2eb1039f9f5f76931f78af7f37d92d28b20c1f58
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2026-04-27 15:43:00 +0200

    feat: readback in lambda value with clousures

--- lib/evals/value/clousure/readback/rbbv.ml
+++ lib/evals/value/clousure/readback/rbbv.ml
@@ -1,1 +1,12 @@
-let rbbv t = failwith "TODO"
+open Core.Syntax
+open Evalapply.Bv
+
+let rec bodies = function
+    | CVar x -> CVar x
+    | CAbs (x, b) ->
+      let b' = bodies @@ bv b in
+      CAbs(x, b')
+  | CApp (m, n) -> CApp (bodies m, bodies n)
+  | _ -> failwith "ERROR: Bodies readback receives a Clou"
+
+let rbbv t = bodies @@ bv t
